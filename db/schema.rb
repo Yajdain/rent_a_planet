e8ea091e5e49c847b3ee485c3de3806524db3044
Yanis Laamiri <yajdain@gmail.com>
2023-02-27 17:29:24 +0100

    navbar, devise, logo

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_142715) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_154039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planet_offers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.float "price"
+    t.string "title"
+    t.string "planet_picture"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.boolean "validated"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +42,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_142715) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
+    t.string "profile_picture"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
