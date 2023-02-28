0fa6c2261a8f864f2f58ea76bd295576b4994001
Yanis Laamiri <yajdain@gmail.com>
2023-02-28 14:31:27 +0100

    modified seeds + planet offer controler synthaxe

--- app/controllers/planet_offers_controller.rb
+++ app/controllers/planet_offers_controller.rb
@@ -1,41 +1,41 @@
 class PlanetOffersController < ApplicationController
 
-before_action :set_planetoffer, only: %i[show edit update destroy]
+before_action :set_planet_offer, only: %i[show edit update destroy]
 
 def index
-  @planetoffers = Planetoffer.all
+  @planet_offers = PlanetOffer.all
 end
 
 def create
-  @planetoffer = Planetoffer.new(planetoffer_params)
-  @planetoffer.save
-  redirect_to planetoffers_path
+  @planet_offer = PlanetOffer.new(planet_offer_params)
+  @planet_offer.save
+  redirect_to planet_offers_path
 end
 
 def update
-  @planetoffer.update(planetoffer_params)
-  redirect_to planetoffer_path(@planetoffer)
+  @planet_offer.update(planet_offer_params)
+  redirect_to planet_offer_path(@planet_offer)
 end
 
 def edit; end
 def show; end
 
 def destroy
-  @planetoffer.destroy
-  redirect_to planetoffers_path, status: :see_other
+  @planet_offer.destroy
+  redirect_to planet_offers_path, status: :see_other
 end
 
 def new
-  @planetoffer = Planetoffer.new
+  @planet_offer = PlanetOffer.new
 end
 
 private
 
-def planetoffer_params
-  params.require(:planetoffer).permit(:title, :name, :description)
+def planet_offer_params
+  params.require(:planet_offer).permit(:title, :name, :description)
 end
 
-def set_planetoffer
-  @planetoffer = Planetoffer.find(params[:id])
+def set_planet_offer
+  @planet_offer = PlanetOffer.find(params[:id])
 end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,82 +3,99 @@
 #
 # Examples:
 
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-planetoffer = PlanetOffer.create(
-  name: "Terre",
-  title: "La planète bleu",
-  description:
-  "Gravité : 9,806 65 m/s²
-  Superficie : 510 067 420 km²
-  Rayon : 6 378,137km
-  Distance du Soleil : 149 millions km",
-  price: "14 millions",
-  planet_picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/560px-The_Blue_Marble_%28remastered%29.jpg"
-)
+puts "Cleaning database..."
+PlanetOffer.destroy_all
 
-planetoffer = PlanetOffer.create(
-  name: "Mars",
-  title: "La planète rouge",
-  description:
-  "Gravité : 3,721 m/s²
-  Superficie : 144,8 millions km²
-  Rayon : 3389,5 km
-  Distance du Soleil : 227,9 millions km'",
-  price: "5 millions",
-  planet_picture: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Mars_Valles_Marineris_EDIT.jpg'
-)
+puts "Creating planets..."
+terre = { name: "Terre", title: "La planète bleue", planet_picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/560px-The_Blue_Marble_%28remastered%29.jpg", price: 14000000, description: "Gravité : 9,806 65 m/s², Superficie : 510 067 420 km², Rayon : 6 378,137km", location: "149 million km away from sun" }
+mars = { name: "Mars", title: "La planète rouge", planet_picture: "https://upload.wikimedia.org/wikipedia/commons/3/36/Mars_Valles_Marineris_EDIT.jpg", price: 5000000, description: "Gravité : 3,721 m/s², Superficie : 144,8 millions km², Rayon : 3389,5 km", location: "227,9 million km away from sun" }
+venus = { name: "Venus", title: "La planète de l'amour", planet_picture: "https://fr.wikipedia.org/wiki/Vénus_%28planète%29#/media/Fichier:Venus-real_color.jpg", price: 10000000, description: "Gravité : 8,87 m/s², Superficie : 460,2 millions km², Rayon : 6051,8 km", location: "108 million km away from sun" }
+saturne = { name: "Saturne", title: "Les sept anneaux", planet_picture: "https://fr.wikipedia.org/wiki/Saturne_%28planète%29#/media/Fichier:Saturn_during_Equinox.jpg", price: 5000000, description: "Superficie : 42,7 milliards km², Distance du Soleil : 1,434 milliard km", location: "5 million km away from sun" }
+pluton = { name: "Pluton", title: "La planète des morts", planet_picture: "https://fr.wikipedia.org/wiki/Pluton_%28planète_naine%29#/media/Fichier:Nh-pluto-in-true-color_2x.jpg", price: 12000000, description: "Gravité : 0,62 m/s², Superficie : 16 650 000 km2, Rayon : 1188,3 km", location: "5,913 million away km from sun" }
+neptune = { name: "Neptune", title: "La planète de la mer", planet_picture: "https://fr.wikipedia.org/wiki/Pluton_%28planète_naine%29#/media/Fichier:Nh-pluto-in-true-color_2x.jpg", price: 7000000, description: "Gravité : 3,721 m/s², Superficie : 144,8 millions km², Rayon : 3389,5 km", location: "227,9 million away km from sun" }
+mercure = { name: "Mercure", title: "La planète de l'intelligence", planet_picture: "https://fr.wikipedia.org/wiki/Mercure_%28planète%29#/media/Fichier:Mercury_in_color_-_Prockter07_centered.jpg", price: 100000000, description: "Gravité : 3,7 m/s², Superficie : 74,8 millions km², Rayon : 2439,7 km", location: "58 million km away from sun" }
 
-planetoffer = PlanetOffer.create(
-  name: "Venus",
-  title: "La planète de l'amour",
-  description: "Gravité : 8,87 m/s²
-  Superficie : 460,2 millions km²
-  Rayon : 6051,8 km
-  Distance du Soleil : 108 000 000 km",
-  price: "10 millions",
-  planet_picture: 'https://fr.wikipedia.org/wiki/Vénus_%28planète%29#/media/Fichier:Venus-real_color.jpg'
-)
+[terre, mars, venus, saturne, pluton, neptune, mercure].each do |attributes|
+  planet_offer = PlanetOffer.create!(attributes)
+  puts "Created #{planet_offer.name}"
+end
+puts 'Finished!'
 
-planetoffer = PlanetOffer.create(
-  name: "Saturne",
-  title: "Les sept anneaux",
-  description: "Superficie : 42,7 milliards km²
-  Distance du Soleil : 1,434 milliard km
-  Durée du jour : 0j 10h 34m",
-  price: "5 millions",
-  planet_picture: 'https://fr.wikipedia.org/wiki/Saturne_%28planète%29#/media/Fichier:Saturn_during_Equinox.jpg'
-)
 
-planetoffer = PlanetOffer.create(
-  name: "Pluton",
-  title: "La planète des morts",
-  description: "Gravité : 0,62 m/s²
-  Superficie : 16 650 000 km2.
-  Rayon : 1188,3 km
-  Distance du Soleil : 5,913 milliards de km",
-  price: "5 millions",
-  planet_picture: 'https://fr.wikipedia.org/wiki/Pluton_%28planète_naine%29#/media/Fichier:Nh-pluto-in-true-color_2x.jpg'
-)
+# planetoffer = PlanetOffer.create(
+#   name: "Mercure",
+#   title: "La planète de l'intelligence",
+#   description: "Gravité : 3,7 m/s², Superficie : 74,8 millions km², Rayon : 2439,7 km
+#   Distance du Soleil : 58 millions de kilomètres",
+#   price: "8 millions",
+#   planet_picture: 'https://fr.wikipedia.org/wiki/Mercure_%28planète%29#/media/Fichier:Mercury_in_color_-_Prockter07_centered.jpg'
 
-planetoffer = PlanetOffer.create(
-  name: "Neptune",
-  title: "La planète de la mer",
-  description: "Gravité : 3,721 m/s²
-  Superficie : 144,8 millions km²
-  Rayon : 3389,5 km
-  Distance du Soleil : 227,9 millions km",
-  price: "7 millions",
-  planet_picture: 'https://fr.wikipedia.org/wiki/Neptune_%28planète%29#/media/Fichier:Neptune.jpg'
-)
 
-planetoffer = PlanetOffer.create(
-  name: "Mercure",
-  title: "La planète de l'intelligence",
-  description: "Gravité : 3,7 m/s²
-  Superficie : 74,8 millions km²
-  Rayon : 2439,7 km
-  Distance du Soleil : 58 millions de kilomètres",
-  price: "8 millions",
-  planet_picture: 'https://fr.wikipedia.org/wiki/Mercure_%28planète%29#/media/Fichier:Mercury_in_color_-_Prockter07_centered.jpg'
-)
+
+
+# planetoffer = PlanetOffer.create(
+#   name: "Neptune",
+#   title: "La planète de la mer",
+#   description: "Gravité : 3,721 m/s², Superficie : 144,8 millions km², Rayon : 3389,5 km
+#   Distance du Soleil : 227,9 millions km",
+#   price: "7 millions",
+#   planet_picture: 'https://fr.wikipedia.org/wiki/Neptune_%28planète%29#/media/Fichier:Neptune.jpg'
+# planetoffer = PlanetOffer.create(
+#   name: "Pluton",
+#   title: "La planète des morts",
+#   description: "Gravité : 0,62 m/s², Superficie : 16 650 000 km2, Rayon : 1188,3 km
+#   Distance du Soleil : 5,913 milliards de km",
+#   price: "5 millions",
+#   planet_picture: 'https://fr.wikipedia.org/wiki/Pluton_%28planète_naine%29#/media/Fichier:Nh-pluto-in-true-color_2x.jpg'
+# )
+
+# planetoffer = PlanetOffer.create(
+#   name: "Saturne",
+#   title: "Les sept anneaux",
+#   description: "Superficie : 42,7 milliards km²
+#   Distance du Soleil : 1,434 milliard km
+#   Durée du jour : 0j 10h 34m",
+#   price: "5 millions",
+#   planet_picture: 'https://fr.wikipedia.org/wiki/Saturne_%28planète%29#/media/Fichier:Saturn_during_Equinox.jpg'
+
+# planetoffer = PlanetOffer.create(
+#   name: "Venus",
+#   title: "La planète de l'amour",
+#   description: "Gravité : 8,87 m/s², Superficie : 460,2 millions km², Rayon : 6051,8 km
+#   Distance du Soleil : 108 000 000 km",
+#   price: "10 millions",
+#   planet_picture: 'https://fr.wikipedia.org/wiki/Vénus_%28planète%29#/media/Fichier:Venus-real_color.jpg'
+
+
+# planetoffer = PlanetOffer.create(
+#   name: "Mars",
+#   title: "La planète rouge",
+#   description:
+#   "Gravité : 3,721 m/s²
+#   Superficie : 144,8 millions km²
+#   Rayon : 3389,5 km
+#   Distance du Soleil : 227,9 millions km'",
+#   price: "5 millions",
+#   planet_picture: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Mars_Valles_Marineris_EDIT.jpg'
+# )
+# planetoffer = PlanetOffer.create(
+#   name: "Terre",
+#   title: "La planète bleu",
+#   description:
+#   "Gravité : 9,806 65 m/s²
+#   Superficie : 510 067 420 km²
+#   Rayon : 6 378,137km
+#   Distance du Soleil : 149 millions km",
+#   price: "14 millions",
+#   planet_picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_Blue_Marble_%28remastered%29.jpg/560px-The_Blue_Marble_%28remastered%29.jpg"
+# )
+
+
+# )
+
+# )
+
+
+# )
+
+# )
