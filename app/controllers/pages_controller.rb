3114e1dcd72b9bb7fc0b41e6b9338e3ab580cd6b
Yanis Laamiri <yajdain@gmail.com>
2023-03-02 15:50:01 +0100

    profile page with link in navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
   def home;end
+
+  def profile
+    @planetoffers = PlanetOffer.all.where("user_id = #{current_user.id}")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :reservations
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/users", to: "users#index"
+  get "/pages/profile", to: "pages#profile"
 end
